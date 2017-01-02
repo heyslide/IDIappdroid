bca9409d7c728a9321ec674df893040374bde2ae
Carlota <carkbra@gmail.com>
2017-01-02 23:40:19 +0100

    Cerca Acabada

--- MyBookDatabase/app/src/main/java/com/example/pr_idi/mydatabaseexample/ByAuthor.java
+++ MyBookDatabase/app/src/main/java/com/example/pr_idi/mydatabaseexample/ByAuthor.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -20,7 +21,7 @@ public class ByAuthor extends AppCompatActivity {
 
     private BookData bookData;
     private Button mFindButton;
-    private Book book;
+    private ListView mlist;
     private static final String TAG = "ByAuthor";
 
     @Override
@@ -30,64 +31,33 @@ public class ByAuthor extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final String nomautor = ((EditText)findViewById(R.id.nomautorinsertat)).getText().toString();
 
-        Toast.makeText(ByAuthor.this, nomautor, Toast.LENGTH_SHORT).show();
-
-        bookData = new BookData(this);
-        bookData.open();
-        final List<Book> values = bookData.getBooksAuthor(nomautor);
-        final ListView list = (ListView) findViewById(R.id.list_autor);
-
-        final ArrayAdapter<Book> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, values);
-        adapter.clear();
-        list.setAdapter(adapter);
+        final String [] prova = new String[] {
+                "hola",
+                "hola2",
+                "funciona porfa"
+        };
 
         mFindButton = (Button)findViewById(R.id.boto);
         mFindButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ArrayAdapter<Book> adapter = (ArrayAdapter<Book>) list.getAdapter();
-                boolean trobat = false;
-                int index = 0;
 
-                String[] newBook = new String[] {
-                        "Miguel Strogoff",
-                        "Jules Verne",
-                        "Ulysses",
-                        "James Joyce",
-                        "Don Quijote",
-                        "Miguel de Cervantes",
-                        "Metamorphosis",
-                        "Kafka"
-                };
+                bookData = new BookData(ByAuthor.this);
+                bookData.open();
+                final String nomautor = ((EditText)findViewById(R.id.nomautorinsertat)).getText().toString();
+                final List<Book> values = bookData.getBooksAuthor(nomautor);
+                mlist = (ListView) findViewById(R.id.list_autor);
+                // Llenar la lista con los valores de values, el adapter sirve para decir tienes
+                // que mostrar estas cosas(Autor, titulo, etc..)
 
-
-                while(!trobat && index < newBook.length) {
-                    if (nomautor.equals(newBook[index])) {
-                        trobat = true;
-                    }
-                    else {
-                        ++index;
-                    }
-                }
+                ArrayAdapter<Book> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, values);
+                //ArrayAdapter adapter = new ArrayAdapter(ByAuthor.this, android.R.layout.simple_list_item_1, prova);
+                //adapter.clear();
+                mlist.setAdapter(adapter);
 
                 Toast.makeText(ByAuthor.this, nomautor, Toast.LENGTH_SHORT).show();
-
-                if (trobat) {
-                    // save the new book to the database
-                    book = bookData.createBook(newBook[index-1], newBook[index]);
-                    // After I get the book data, I add it to the adapter
-                    adapter.add(book);
-                }
-
-                else {
-                    Toast.makeText(ByAuthor.this, "Author doesn't exist", Toast.LENGTH_SHORT).show();
-                }
-
             }
         });
-
     }
 }
