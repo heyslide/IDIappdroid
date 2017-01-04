2228c9625bccdaf33714a8653d72cae29ae05d14
Carlota <carkbra@gmail.com>
2017-01-04 13:08:39 +0100

    Casi arreglat problema de afegir

--- MyBookDatabase/app/src/main/java/com/example/pr_idi/mydatabaseexample/SearchAuthor.java
+++ MyBookDatabase/app/src/main/java/com/example/pr_idi/mydatabaseexample/SearchAuthor.java
@@ -5,12 +5,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SearchAuthor extends Fragment {
 
+    private BookData bookData;
+    private Button mFindButton;
+    private ListView mlist;
+    private static final String TAG = "ByAuthor";
 
     public SearchAuthor() {
         // Required empty public constructor
@@ -21,7 +32,38 @@ public class SearchAuthor extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_author, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_search_author, container, false);
 
+        final EditText nom_autor = (EditText) v.findViewById(R.id.nomautorinsertat);
+
+        mFindButton = (Button) v.findViewById(R.id.boto);
+        mFindButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+               bookData = new BookData(getActivity());
+               bookData.open();
+
+               if (nom_autor.getText().toString().isEmpty()) {
+                   Toast.makeText(getActivity(), "The field is Requiered", Toast.LENGTH_SHORT).show();
+               }
+
+               else {
+                   String nomautor = nom_autor.getText().toString();
+                   final List<Book> values = bookData.getBooksAuthor(nomautor);
+                   mlist = (ListView) v.findViewById(R.id.list_autor);
+
+                   ArrayAdapter<Book> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, values);
+                   mlist.setAdapter(adapter);
+
+                   if (adapter.isEmpty()) {
+                       Toast.makeText(getActivity(), "The Author " + nomautor + " is not at the DataBase", Toast.LENGTH_SHORT).show();
+                   }
+               }
+           }
+        });
+
+        return v;
+
+    }
 }
